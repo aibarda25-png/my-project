1fb4077ef3cd41a01a81df3fb37c7a81c3ab6320
Aibar Diushenov <aibarda25@kstu.kg>
2026-03-23 20:43:35 +0600

    Add main.cpp with basic I/O and loop example

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, this is Aibar project" << endl;
+
+    int a = 5;
+    int b = 10;
+
+    cout << "Sum: " << a + b << endl;
+
+    for(int i = 0; i < 5; i++) {
+        cout << "i = " << i << endl;
+    }
+
+    return 0;
+}
